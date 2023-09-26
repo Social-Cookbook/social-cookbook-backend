3bb0e6c219df34b57aa85cddf4c6d85352914c49
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2023-09-26 16:09:18 -0400

    Made minor changes to the router and server.js to fix api bug. Changed router organization. Added a few dependencies to package file.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import express from "express"
 import cors from "cors"
-import recipe_posts from "./api/recipe_posts.route.js"
+import recipe_posts_router from "./api/recipe_posts/route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/v1/recipe_posts", recipe_posts)
-app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
+app.use("/api/recipe-posts", recipe_posts_router)
+app.use("*", (req, res) => res.status(404).json())
 
 export default app
-
