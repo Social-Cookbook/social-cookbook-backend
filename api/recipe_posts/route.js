2f8c85b39ca54a7e45e8a12ae542d92d9ce4a71a
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2023-10-01 18:19:13 -0400

    Created backend functionality for basic get query that gets recipe-posts from the database.

--- api/recipe_posts/route.js
+++ api/recipe_posts/route.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import Recipe_Posts_Controller from "./controller.js"
 
 const recipe_posts_router = express.Router();
 
-recipe_posts_router.route("/").get((req, res) => {
-    res.send("Hello World");
-});
+recipe_posts_router.route("/").get(Recipe_Posts_Controller.apiGetPosts);
 
 recipe_posts_router.route("/create").post((req, res) => {
     const { a } = req.body
