cb25bcaa596164bbf0310cb7cd6e42b64a9d81e4
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2023-10-10 16:58:31 -0400

    Created endpoints for creating, updating, deleting recipe posts

--- api/recipe_posts/dao.js
+++ api/recipe_posts/dao.js
@@ -1,3 +1,6 @@
+import mongodb from 'mongodb'
+const ObjectId = mongodb.ObjectId
+
 let recipe_posts
 
 export default class Recipe_posts_DAO {
@@ -12,11 +15,12 @@ export default class Recipe_posts_DAO {
         }
     }
 
-    static async getRecipePosts({
+    static async getPosts({
         filters = null,
         page = 0,
         postsPerPage = 20,
     } = {}) {
+
         let query
         if (filters) {
             if ("name" in filters) {
@@ -36,14 +40,128 @@ export default class Recipe_posts_DAO {
         }
 
         const displayCursor = cursor.limit(postsPerPage).skip(postsPerPage * page)
-
         try {
             const postsList = await displayCursor.toArray()
             const totalNumPosts = await recipe_posts.countDocuments(query)
+
             return { postsList, totalNumPosts }
         } catch (e) {
             console.error(`Unable to convert cursor to array or problem counting documents, ${e}`)
             return { postsList: [], totalNumPosts: 0 }
         }
     }
+
+    static async getPostById(postId) {
+        const query = { "id" : postId }
+        try {
+            return await recipe_posts.findOne(query)
+        } catch (e) {
+            console.error(`Unable to issue find command: ${e}`)
+            return []
+        }
+    }
+
+    static async addPost(post, userID) {
+        try {
+            const new_post = {
+                id: post.id,
+                title: post.title,
+                description: post.description,
+                steps: post.steps,
+                ingredients: post.ingredients,
+                totalPrice: post.totalPrice,
+                photoURLs: post.photoURLs,
+                userId: new ObjectId(userID),
+            }
+
+            return await recipe_posts.insertOne(new_post)
+        } catch (e) {
+            console.error(`Unable to create post: ${e}`)
+            return { error : e }
+        }
+    }
+
+    static async updatePost(post, userId) {
+        try {
+            const updateResponse = await recipe_posts.updateOne(
+                {
+                    id: post.id,
+                    userId: new ObjectId(userId),
+                },
+                {
+                    $set: {
+                        title: post.title,
+                        description: post.description,
+                        steps: post.steps,
+                        ingredients: post.ingredients,
+                        totalPrice: post.totalPrice,
+                        photoURLs: post.photoURLs,
+                    }
+                }
+            )
+
+            return updateResponse
+        } catch (e) {
+            console.error(`Unable to update post: ${e}`)
+            return { error : e }
+        }
+    }
+
+    static async updatePostById(post, postId, userId) {
+        try {
+            const updateResponse = await recipe_posts.updateOne(
+                {
+                    id: postId,
+                    userId: new ObjectId(userId),
+                },
+                {
+                    $set: {
+                        title: post.title,
+                        description: post.description,
+                        steps: post.steps,
+                        ingredients: post.ingredients,
+                        totalPrice: post.totalPrice,
+                        photoURLs: post.photoURLs,
+                    }
+                }
+            )
+
+            return updateResponse
+        } catch (e) {
+            console.error(`Unable to update post: ${e}`)
+            return { error : e }
+        }
+    }
+
+    static async deletePost(postId, userId) {
+        try {
+            const deleteResponse = await recipe_posts.deleteOne(
+                {
+                    id: postId,
+                    userId: new ObjectId(userId),
+                }
+            )
+
+            return deleteResponse
+        } catch (e) {
+            console.error(`Unable to delete post: ${e}`)
+            return { error : e }
+        }
+    }
+
+    // static async deletePostById(postId, userId) {
+    //     try {
+    //         const deleteResponse = await recipe_posts.deleteOne(
+    //             {
+    //                 id: postId,
+    //                 userId: new ObjectId(userId),
+    //             }
+    //         )
+
+    //         return deleteResponse
+    //     } catch (e) {
+    //         console.error(`Unable to delete post: ${e}`)
+    //         return { error : e }
+    //     }
+    // }
 }
--- api/recipe_posts/route.js
+++ api/recipe_posts/route.js
@@ -4,9 +4,11 @@ import Recipe_Posts_Controller from "./controller.js"
 const recipe_posts_router = express.Router();
 
 recipe_posts_router.route("/").get(Recipe_Posts_Controller.apiGetPosts);
-
-recipe_posts_router.route("/create").post((req, res) => {
-    const { a } = req.body
-})
+recipe_posts_router.route("/:postId").get(Recipe_Posts_Controller.apiGetPostById);
+recipe_posts_router.route("/create").post(Recipe_Posts_Controller.apiCreatePosts);
+recipe_posts_router.route("/update").put(Recipe_Posts_Controller.apiUpdatePosts);
+recipe_posts_router.route("/:postId/update").put(Recipe_Posts_Controller.apiUpdatePostById);
+recipe_posts_router.route("/delete").delete(Recipe_Posts_Controller.apiDeletePosts);
+recipe_posts_router.route("/:postId/delete").delete(Recipe_Posts_Controller.apiDeletePostById);
 
 export default recipe_posts_router;
