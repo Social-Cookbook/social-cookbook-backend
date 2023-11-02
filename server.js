9a995798fd73d5ae76ecc471af2e1ff9969edbfe
edimarco3 <edimarco3@gatech.edu>
2023-11-02 11:58:41 -0400

    Moved the upload functionality to the router

--- server.js
+++ server.js
@@ -12,23 +12,23 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const s3 = new AWS.S3();
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'social-cookbook-images',
-        key: function (req, file, cb) {
-            cb(null, Date.now().toString())
-        }
-    })
-});
-
-app.post('/upload', upload.single('image'), function (req, res, next) {
-    console.log(req.file.originalname);
-});
+// const s3 = new AWS.S3();
 
 const app = express()
+
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: 'social-cookbook-images',
+//         key: function (req, file, cb) {
+//             cb(null, Date.now().toString())
+//         }
+//     })
+// });
+
+// app.post('/upload', upload.single('image'), function (req, res, next) {
+//     console.log(req.file.originalname);
+// });
 
 app.use(cors())
 app.use(express.json())
