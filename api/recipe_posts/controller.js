19bbc3c9388ac4c4277b36dbc5f1f910e1ead250
edimarco3 <edimarco3@gatech.edu>
2023-11-07 14:19:28 -0500

    Added the upload image api endpoint to the router

--- api/recipe_posts/controller.js
+++ api/recipe_posts/controller.js
@@ -1,7 +1,7 @@
 import Recipe_posts_DAO from "./dao.js";
-import multer from "multer"
-import AWS from "aws-sdk"
-import multerS3 from "multer-s3"
+// import multer from "multer"
+// import AWS from "aws-sdk"
+// import multerS3 from "multer-s3"
 
 export default class Recipe_Posts_Controller {
     static async apiGetPosts(req, res, next) {
@@ -197,27 +197,29 @@ export default class Recipe_Posts_Controller {
         }
     }
 
-    static async apiUploadImage(req, res, next) {
-        try {
-//             AWS.config.update({
-//                 accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-//             });
-            const s3 = new AWS.S3();
-            const upload = multer({
-                storage: multerS3({
-                    s3: s3,
-                    bucket: 'social-cookbook-images',
-                    key: function (req, file, cb) {
-                        cb(null, Date.now().toString())
-                    }
-                })
-            });
-            app.post('/upload', upload.single('image'), function (req, res, next) {
-                console.log(req.file.originalname);
-            });
-        } catch (e) {
-            res.status(500).json({ error: e.message })
-        }
-    }
+    // static async apiUploadImage(req, res, next) {
+    //     try {
+    //         AWS.config.update({
+    //             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    //         });
+    //         const s3 = new AWS.S3();
+    //         const upload = multer({
+    //             storage: multerS3({
+    //                 s3: s3,
+    //                 bucket: 'social-cookbook-images',
+    //                 key: function (req, file, cb) {
+    //                     cb(null, Date.now().toString())
+    //                 }
+    //             })
+    //         });
+    //         // app.post('/upload', upload.single('image'), function (req, res, next) {
+    //         //     console.log(req.file.originalname);
+    //         // });
+    //         upload.single('image');
+    //         res.json({ status: "success" })
+    //     } catch (e) {
+    //         res.status(500).json({ error: e.message })
+    //     }
+    // }
 }
--- server.js
+++ server.js
@@ -3,32 +3,8 @@ import cors from "cors";
 import recipe_posts_router from "./api/recipe_posts/route.js";
 import user_info_router from "./api/user_info/route.js";
 import followers_info_router from "./api/followers/followersRoute.js";
-import multer from "multer"
-import AWS from "aws-sdk"
-import multerS3 from "multer-s3"
-
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-
-// const s3 = new AWS.S3();
 
 const app = express()
-
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'social-cookbook-images',
-//         key: function (req, file, cb) {
-//             cb(null, Date.now().toString())
-//         }
-//     })
-// });
-
-// app.post('/upload', upload.single('image'), function (req, res, next) {
-//     console.log(req.file.originalname);
-// });
 
 app.use(cors())
 app.use(express.json())
