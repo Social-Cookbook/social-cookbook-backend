98654bede7e986c1b29e35fd84ec924d5091fe7e
Ian Wood <ianwood103@gmail.com>
2023-11-07 19:18:29 -0500

    fix merge conflict errors

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import app from "./server.js";
 import mongodb from "mongodb";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import Recipe_posts_DAO from "./api/recipe_posts/dao.js";
 import UserDataDAO from "./api/user_info/userDAO.js";
 import Followers_DAO from "./api/followers/followersDAO.js";
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
-
 
 const port = process.env.PORT;
 
@@ -24,30 +23,28 @@ MongoClient.connect(process.env.SC_DB_URI, {
     await UserDataDAO.injectDB(client);
     await Recipe_posts_DAO.injectDB(client);
     await Followers_DAO.injectDB(client);
-
+  });
 
 mongoose
-    .connect(process.env.SC_DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB is  connected successfully"))
-    .catch((err) => console.error(err));
+  .connect(process.env.SC_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
 
-MongoClient.connect(
-    process.env.SC_DB_URI,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-        useNewUrlParser: true
-    }
-).catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-}).then(async client => {
-    await Recipe_posts_DAO.injectDB(client)
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
-    })
+MongoClient.connect(process.env.SC_DB_URI, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+  useNewUrlParser: true,
 })
-
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await Recipe_posts_DAO.injectDB(client);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  });
