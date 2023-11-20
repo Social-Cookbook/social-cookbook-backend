b7bae29e2b46bcc331d0801ae84c46f489800441
edimarco3 <edimarco3@gatech.edu>
2023-11-20 17:59:57 -0500

    Added an api endpoint to get users by ID

--- api/user_info/route.js
+++ api/user_info/route.js
@@ -6,5 +6,6 @@ const user_info_router = express.Router();
 user_info_router.route("/").get(User_Data_Controller.apiGetUsers);
 user_info_router.route("/create").post(User_Data_Controller.apiCreateUsers);
 user_info_router.route("/update").put(User_Data_Controller.apiUpdateUsers);
+user_info_router.route("/:userId").get(User_Data_Controller.apiGetUserById);
 
 export default user_info_router;
