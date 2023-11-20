71d9fd4e09ea35f3651b5908b5b9cae0649384db
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2023-11-20 18:42:01 -0500

    Added endpoint for userpage.

--- api/user_info/route.js
+++ api/user_info/route.js
@@ -5,6 +5,7 @@ const user_info_router = express.Router();
 
 user_info_router.route("/").get(User_Data_Controller.apiGetUsers);
 user_info_router.route("/:userId").get(User_Data_Controller.apiGetUserById);
+user_info_router.route("/userpage/:userId").get(User_Data_Controller.apiGetUserPageInfoById);
 user_info_router.route("/create").post(User_Data_Controller.apiCreateUsers);
 user_info_router.route("/update").put(User_Data_Controller.apiUpdateUsers);
 
