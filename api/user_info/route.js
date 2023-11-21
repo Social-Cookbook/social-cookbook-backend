5a38abdb05e59b1628ecb8a77eb64bfbae0b0575
Ian Wood <ianwood103@gmail.com>
2023-11-21 15:48:35 -0500

    made backend endpoint use current userid

--- api/user_info/route.js
+++ api/user_info/route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import User_Data_Controller from "./userController.js"
+import userVerification from '../user_system/AuthMiddleware.js';
 
 const user_info_router = express.Router();
 
 user_info_router.route("/").get(User_Data_Controller.apiGetUsers);
 user_info_router.route("/:userId").get(User_Data_Controller.apiGetUserById);
-user_info_router.route("/userpage/:userId").get(User_Data_Controller.apiGetUserPageInfoById);
+user_info_router.post("/userpage", userVerification, User_Data_Controller.apiGetUserPageInfoById);
 user_info_router.route("/create").post(User_Data_Controller.apiCreateUsers);
 user_info_router.route("/update").put(User_Data_Controller.apiUpdateUsers);
 user_info_router.route("/:userId").get(User_Data_Controller.apiGetUserById);
