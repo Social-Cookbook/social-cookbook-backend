c6051c5f31c8c62c56d29c2a72a73bcb91a09af4
Ian Wood <ianwood103@gmail.com>
2023-12-23 23:53:33 -0500

    update cors frontend origin

--- server.js
+++ server.js
@@ -8,20 +8,19 @@ import user_info_router from "./api/user_info/route.js";
 import followers_info_router from "./api/followers/followersRoute.js";
 import following_info_router from "./api/following/followingRoute.js";
 
-
-const app = express()
+const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3001', // The exact origin of the frontend
-    credentials: true, // This allows the server to accept cookies from the frontend
+  origin: "https://social-cookbook-frontend.vercel.app/", // The exact origin of the frontend
+  credentials: true, // This allows the server to accept cookies from the frontend
 };
 
-app.use(cors(corsOptions))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use(cookieParser());
 app.use("/", authRoute);
-app.use("/api/auth", auth_router)
+app.use("/api/auth", auth_router);
 app.use("/api/recipe-posts", recipe_posts_router);
 app.use("/api/followers", followers_info_router);
 app.use("/api/following", following_info_router);
